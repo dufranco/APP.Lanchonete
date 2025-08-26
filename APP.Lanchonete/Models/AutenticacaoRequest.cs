f555603bb65ebd6081f854f6b362062559ccff3c
Eduardo Franco de Lima <edu.developer@outlook.com>
2025-08-26 20:18:02 -0300

    #00003 - Implementado login do usuário

---
+++ APP.Lanchonete/Models/AutenticacaoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace APP.Lanchonete.Models
+{
+    public class AutenticacaoRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+    }
+}
