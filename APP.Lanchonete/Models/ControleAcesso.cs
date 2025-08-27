bdc3dc4266efe1620834915dc91828ec156806f8
Eduardo Franco de Lima <edu.developer@outlook.com>
2025-08-26 23:58:50 -0300

    #00004 - Implementado controle de acessos

---
+++ APP.Lanchonete/Models/ControleAcesso.cs
@@ -0,0 +1,15 @@
+﻿namespace APP.Lanchonete.Models
+{
+    public class ControleAcesso
+    {
+        public int IdControle { get; set; }
+
+        public int IdPerfil { get; set; }
+
+        public string NomePerfil { get; set; }
+
+        public string NomeTela { get; set; }
+
+        public bool Permitido { get; set; }
+    }
+}
