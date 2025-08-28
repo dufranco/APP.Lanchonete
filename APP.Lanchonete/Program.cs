6df0f6832fd383a637f73527b793e886b0c8993b
Eduardo Franco de Lima <edu.developer@outlook.com>
2025-08-28 02:27:04 -0300

    #00008 - Adicionado arquivo railway.toml para considerar buildar usando o Dockerfile e config para healthcheck

--- APP.Lanchonete/Program.cs
+++ APP.Lanchonete/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHealthChecks();
+
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
@@ -17,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseHealthChecks("/health");
 
 app.UseHttpsRedirection();
 
