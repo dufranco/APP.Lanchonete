1d2749027504e23fd6008b6e656547e74ba1d15b
Eduardo Franco de Lima <edu.developer@outlook.com>
2025-08-27 22:45:13 -0300

    #00006 - Ajuste no cadastro de pedidos

--- APP.Lanchonete/Models/ItemPedido.cs
+++ APP.Lanchonete/Models/ItemPedido.cs
@@ -1,11 +1,22 @@
-﻿namespace APP.Lanchonete.Models
+﻿using System.Text.Json.Serialization;
+
+namespace APP.Lanchonete.Models
 {
     public class ItemPedido
     {
+        [JsonPropertyName("idItem")]
         public int IdItem { get; set; }
+
+        [JsonPropertyName("idProduto")]
         public int IdProduto { get; set; }
+
+        [JsonPropertyName("quantidade")]
         public int Quantidade { get; set; }
+
+        [JsonPropertyName("descricaoProduto")]
         public string DescricaoProduto { get; set; }
+
+        [JsonPropertyName("status")]
         public string Status { get; set; }
     }
 }
